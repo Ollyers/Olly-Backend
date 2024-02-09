ff8ce8cee40bafd3472b777f069b4ef11656fa68
skydreamer21 <skydreamer210@gmail.com>
2024-02-09 14:21:10 +0900

    chore: init project

---
+++ src/main/java/com/skygame/olly/OllyApplication.java
@@ -0,0 +1,13 @@
+package com.skygame.olly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skygame/olly/OllyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skygame.olly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
