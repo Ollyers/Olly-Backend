0423d8be303e65181ae5f87a4d9bbae12d23839e
skydreamer21 <skydreamer210@gmail.com>
2024-02-11 18:05:14 +0900

    feat: member entity 간단 구현

---
+++ src/main/java/com/skygame/olly/member/domain/Member.java
@@ -0,0 +1,26 @@
+package com.skygame.olly.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int level;
+}
